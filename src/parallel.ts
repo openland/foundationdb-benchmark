2a38c93a95f83150c0e53714739bb5440753ec4c
Steve Korshakov <steve@korshakov.com>
2020-05-20 20:49:34 -0700

    feat: More benchmarks

---
+++ src/parallel.ts
@@ -0,0 +1,44 @@
+import * as fdb from 'foundationdb';
+// let loopLag = require('event-loop-lag')(100);
+// setInterval(() => {
+//     console.log(loopLag());
+// }, 100);
+
+export function currentTime() {
+    let t = process.hrtime();
+    return ((t[0] * 1e9) + t[1]) / 1000000;
+}
+
+(async () => {
+    fdb.setAPIVersion(620);
+    let dbs: fdb.Database<fdb.TupleItem[], fdb.TupleItem[], string, string>[] = [];
+    for (let k = 0; k < 1; k++) {
+        let db = fdb.open();
+        let dir = await fdb.directory.createOrOpen(db, ['fdb-benchmark']);
+        let sp = db.at(dir)
+            .withKeyEncoding(fdb.encoders.tuple)
+            .withValueEncoding(fdb.encoders.string);
+        dbs.push(sp);
+    }
+    let start = currentTime();
+    let pending: Promise<void>[] = [];
+    for (let k = 0; k < dbs.length; k++) {
+        let sp = dbs[k];
+        let rw = await sp.doTn(async txn => {
+            return txn.getReadVersion();
+        })
+        for (let j = 0; j < 100; j++) {
+            pending.push(sp.doTn(async txn => {
+                txn.setReadVersion(rw);
+                let pk: Promise<string | undefined>[] = [];
+                for (let i = 0; i < 1000; i++) {
+                    pk.push(txn.get([i]));
+                }
+                await Promise.all(pk);
+            }));
+        }
+    }
+    await Promise.all(pending);
+    let delta = currentTime() - start;
+    console.warn(delta + ' ms');
+})();
---
+++ src/write.ts
@@ -0,0 +1,25 @@
+import * as fdb from 'foundationdb';
+
+export function currentTime() {
+    let t = process.hrtime();
+    return ((t[0] * 1e9) + t[1]) / 1000000;
+}
+
+(async () => {
+    fdb.setAPIVersion(620);
+    let db = fdb.open();
+    let dir = await fdb.directory.createOrOpen(db, ['fdb-benchmark']);
+    let sp = db.at(dir)
+        .withKeyEncoding(fdb.encoders.tuple)
+        .withValueEncoding(fdb.encoders.string);
+    let start = currentTime();
+    let pending: Promise<void>[] = [];
+    for (let j = 0; j < 100000; j++) {
+        pending.push(sp.doTn(async txn => {
+            txn.set([j], 'hello');
+        }));
+    }
+    await Promise.all(pending);
+    let delta = currentTime() - start;
+    console.warn(delta + ' ms');
+})();
